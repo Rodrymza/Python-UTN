b3610b32b4995abc81b75cb1257dc5caec05109e
Rodry <rodrymza@gmail.com>
2024-09-27 16:27:29 -0300

    Reorganizacion de archivos

--- Borrador2.py
+++
@@ -1,9 +0,0 @@
-nombre = "Rodrigo   Ramirez  "
-nuevo_nombre=""
-for i in range(len(nombre)-1):
-    if nombre[i]==" " and nombre[i+1]==" ":
-        i+=1
-    else:
-        nuevo_nombre+=nombre[i]
-
-print(nuevo_nombre)
