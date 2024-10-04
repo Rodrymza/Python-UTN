f96688407cff810617ab9ac28bc5eebc833c76dd
Rodry <rodrymza@gmail.com>
2024-10-04 17:13:40 -0300

    Finalizacion de ejercicio

--- Modelo Primer Examen/Modelo Examen.py
+++ Modelo Primer Examen/Modelo Examen.py
@@ -13,7 +13,6 @@ def pedir_valores(valor_min):
 
 def crear_matriz(filas,columnas, opcion):
     matriz = []
-    
     for _ in range(filas):
         fila = []
         for _ in range(columnas):
@@ -36,46 +35,50 @@ def sumar_filas(matriz):
     matriz_suma = []
     for filas in matriz:
         suma = 0
-        lista_suma = []
         for elemento in filas:
             suma += elemento
-        lista_suma.append(suma)
-        matriz_suma.append(lista_suma)
-    
+        matriz_suma.append([round(suma,2)])
     return matriz_suma        
-            
 
-print("Ingrese el numero de filas ")
-filas = pedir_valores(filas_min)
-print("Ingrese el numero de columnas")
-columnas = pedir_valores(columnas_min)
+def agregar_columna(matriz):
+    i=1
+    for filas in matriz:
+            filas.append(i)
+            i += 1    
+    ordenar(matriz)
+    return matriz
 
-ingreso = ""
+def ordenar(lista):
+    for i in range(len(lista)):
+        for j in range(i+1,len(lista)):
+            if lista[i]<lista[j]:
+                auxiliar = lista[i]
+                lista[i] = lista[j]
+                lista[j] = auxiliar
+
+#pedir valores de columnas y filas
+#print("Ingrese el numero de filas ")
+#filas = pedir_valores(filas_min)
+#print("Ingrese el numero de columnas")
+#columnas = pedir_valores(columnas_min)
+filas, columnas = 5, 5
 print("¿Desea colocar los valores automaticamente o de forma manual?")
 print("1 Manual")
 print("2 Automatico")
-while ingreso!="1" and ingreso!="2":
-    ingreso = str(input())
-    match ingreso:
-        case "1" | "2": matriz_numeros = crear_matriz(filas, columnas, ingreso) 
-        
-        case _: print("Opcion incorrecta (1-2)")
-        
+
+ingreso = "2"
+while ingreso not in ("1","2"):    
+    print("Opcion incorrecta (1-2)")
+    ingreso = input()
+
+matriz_numeros = crear_matriz(filas, columnas,ingreso)
+print("Matriz original:")    
 mostrar_matriz(matriz_numeros)
 
 filas_sumadas = sumar_filas(matriz_numeros)
-
+print("Filas sumadas")
 mostrar_matriz(filas_sumadas)
-lista_auxiliar = []
-for fila in filas_sumadas:
-    for elemento in fila:
-        lista_auxiliar.append(elemento)
-    lista_auxiliar.sort(reverse = True)
 
-matriz_nueva = []
-
-for elemento in lista_auxiliar:
-    matriz_nueva
-    
-print("nueva matriz")
-mostrar_matriz(matriz_nueva)
+print("Columna agregada de posicion inicial:")
+matriz_indice = agregar_columna(filas_sumadas)
+mostrar_matriz(matriz_indice)
