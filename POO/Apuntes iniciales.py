624281c14fb98ff519397cc6486447d6ac7d6578
Rodry <rodrymza@gmail.com>
2024-10-27 13:58:52 -0300

    actualizacion de ejercicio

--- POO/Apuntes iniciales.py
+++ POO/Apuntes iniciales.py
@@ -10,3 +10,9 @@ class Alumno:
 alumno1 = Alumno("Juan", "Alonso", 25000)
 
 alumno1.mostrar_datos()
+
+"""Encapsulamiento: todo lo que haga referencia a la clase se trate dentro de la misma clase (metodos y atributos)
+y no debe exponerse. Esto se logra ocultando los atributos
+Herencia: se crean nuevas clases a partir de clases ya existentes
+Polimorfismo: diferentes clases pueden ser tratadas como si fueran del mismo tipo a traves de metodos con el mismo nombre (debe haber herencia primero)
+Abstraccion: tratar de modelar todos los objetos a traves de la clase, agrupar todos los atributos unicos en los objetos"""
--- POO/Ejercicio01.py
+++ POO/Ejercicio01.py
@@ -1,34 +1,46 @@
 class Celdas:
-    
-    fila = 0
-    columna = 0
-    valor = ""
-    
-    @staticmethod
-    def ingresar_numero():
+    def __init__(self):
+        self.fila = 0
+        self.columna = 0
+        self.valor = ""
+        
+    def ingresar_numero(self, mensaje = "Ingrese un numero"):
         while True:
-            numero = input("Ingrese un numero\n")
+            numero = input(f"{mensaje} \n")
             if numero.isnumeric():
                 return int(numero)
             else:
                 print("Ingresaste un valor incorrecto")
+    
     def pedir_valores(self):
-        print("Ingrese la fila")
-        self.fila = self.ingresar_numero()
-        print("Ingrese la columna")
-        self.columna = self.ingresar_numero()
-        self.valor = input(f"Ingrese valor a colocar en la fila {self.fila} columna {self.columna}\n")
-        print("Valor ingresado correctamente")
+        self.fila = self.ingresar_numero("Ingrese la fila")
+        self.columna = self.ingresar_numero("Ingrese la columna")
         
     def agregar_matriz(self, matriz):
-        matriz.agregar_celdas([self.fila, self.columna, self.valor])
+        if not matriz.celda_ocupada(self.fila, self.columna):
+            self.valor = input(f"Ingrese valor a colocar en la fila {self.fila} columna {self.columna}\n")
+            print("Valor ingresado correctamente")
+
+            matriz.agregar_celda([self.fila, self.columna, self.valor])
+        else:
+            print("Esta celda ya se encuentra ocupada")
             
     def obtener_valor(self):
         return self.valor
+    
+
 class Matriz:
-    matriz = []
-    def agregar_celdas(self, valor):
-        if valor[2] != "salir":
+    def __init__(self):
+        self.matriz = []
+        
+    def celda_ocupada(self, fila, columna):
+        for celda in self.matriz:
+            if celda[0] == fila and celda[1] == columna:
+                return True
+        return False
+    
+    def agregar_celda(self, valor):
+        if valor[2] != "fin":
             self.matriz.append(valor)
     
     def ordenar_matriz(self):
@@ -39,14 +51,19 @@ class Matriz:
         print("Celdas:")
         for elemento in self.matriz:
             print(f"Fila {elemento[0]} Columna: {elemento[1]} Valor: {elemento[2]}")
+            
+   
+def main():
+    matriz_usuario = Matriz()
 
-matriz_usuario = Matriz()
+    while True:
+        celda = Celdas()
+        celda.pedir_valores()
+        if celda.obtener_valor().lower() == "fin":
+            break
+        celda.agregar_matriz(matriz_usuario)
 
-while True:
-    celda = Celdas()
-    celda.pedir_valores()
-    if celda.obtener_valor().lower() == "salir":
-        break
-    celda.agregar_matriz(matriz_usuario)
-
-matriz_usuario.mostrar_celdas()
+    matriz_usuario.mostrar_celdas()
+    
+if __name__ == "__main__":
+    main()
